13584d45225701dd4dd4c4f499ca01c022800695
Ryan-Pi <68978091+Ryan-Pi@users.noreply.github.com>
2022-05-30 00:22:47 +1000

    new algo work

--- src/newAlgo.java
+++ src/newAlgo.java
@@ -21,49 +21,67 @@ public class newAlgo extends Scheduler{
 		write("OK");
 	}
 	
-	public void findServer(int numberOfServers) {
+//	public void findServer(int numberOfServers) {
 //		boolean allOn = true;
-		ArrayList<Server> availableServers = new ArrayList<Server>();
-		//request all servers with resources currently available to execute the job
-		for(int i = 0; i < numberOfServers; i++) {
-			read();
-			Server server = new Server(str);
-			availableServers.add(server);
+//		ArrayList<Server> availableServers = new ArrayList<Server>();
+//		//request all servers with resources currently available to execute the job
+//		for(int i = 0; i < numberOfServers; i++) {
+//			read();
+//			Server server = new Server(str);
+//			availableServers.add(server);
 //			if(server.getServerStatus().equals("inactive")) {
 //				allOn = false;
 //			}
-		}
-		//if there are available servers, schedule to the first such server
-		if(availableServers.size()>0) {
-			targetServer = availableServers.get(0);
-		} else {
-			//find a server capable of running the job
-			write("GETS Capable " + job.getCores() + " " + job.getDisk() + " " + job.getMemory());
-			read();
-			String[] serverStr = str.split(" ",3);
-			int serverNo = Integer.valueOf(serverStr[1]);
-			write("OK");
-			ArrayList<Server> capableServers = new ArrayList<Server>();
-			for(int i = 0; i < serverNo; i++) {
-				read();
-				Server server = new Server(str);
-				capableServers.add(server);
-			}
-			write("OK");
-			waitFor(".");
-			//look for server that has the lowest estimated wait time
-			int waitTime = 99999999;
-			for(int i = 0; i < capableServers.size();i++) {
-				Server server = capableServers.get(i);
-				write("EJWT " + server.getServerType() + " " + server.getServerID());
-				read();
-				int time = Integer.valueOf(str);
-				if(time<waitTime) {
-					targetServer = server;
-				}
-				write("OK");
-			}
-		}
+//		}
+//		//if there are available servers, schedule to the first such server
+//		if(availableServers.size()>0) {
+//			if(!allOn) {
+//				targetServer = availableServers.get(0);
+//			} else {
+//				int fit = -1;
+//				for(int i = 0; i < availableServers.size();i++) {
+//					Server server = availableServers.get(i);
+//					int serverFit = server.getServerCoresInt()-job.getCoresInt();
+//					if(serverFit>fit) {
+//						targetServer = server;
+//					}
+//				}
+//			}
+//		}else {
+//			//find a server capable of running the job
+//			write("GETS Capable " + job.getCores() + " " + job.getDisk() + " " + job.getMemory());
+//			while(!str.contains("DATA")) {
+//				read();
+//			}
+//			String[] serverStr = str.split(" ",3);
+//			int serverNo = Integer.valueOf(serverStr[1]);
+//			write("OK");
+//			ArrayList<Server> capableServers = new ArrayList<Server>();
+//			for(int i = 0; i < serverNo; i++) {
+//				read();
+//				Server server = new Server(str);
+//				capableServers.add(server);
+//			}
+//			read();
+//			write("OK");
+//			//look for server that has the lowest estimated wait time
+//			int waitTime = 99999999;
+//			for(int i = 0; i < capableServers.size();i++) {
+//				Server server = capableServers.get(i);
+//				write("EJWT " + server.getServerType() + " " + server.getServerID());
+//				read();
+//				int time = Integer.valueOf(str);
+//				if(time<waitTime) {
+//					targetServer = server;
+//				}
+//				write("OK");
+//				waitFor(".");
+//			}
+//		}
+//	}
+	
+	public void findServer() {
+		
 	}
 	
 	@Override
